0a0e0795fb1ed369682295fda19a12151e303ed1
Kristian Weegink <k.weegink@gmail.com>
2021-09-20 11:53:46 +1000

    Turned into python package
    * created pydebug package
    * refactored out the data types/enums/unions into seperate file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='pydebug',
+      version='0.0',
+      description='Python Distribution Utilities',
+      author='Kristian Weegink',
+      author_email='k.weegink@gmail.com',
+      url='',
+      packages=['pydebug'],
+     )
